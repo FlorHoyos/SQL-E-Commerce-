2fff9a39ff6590bdeee2ae4f0938796440894443
Flor Hoyos <79953103+FlorHoyos@users.noreply.github.com>
2026-01-25 15:11:34 -0600

    Update sellers_and_fulfillment.sql

--- sellers_and_fulfillment.sql
+++ sellers_and_fulfillment.sql
@@ -1,15 +1,20 @@
+-- create a formula that calculates delivered orders per seller
+-- left join to keep sellers even if they have no order items
+-- join only delivered orders only
+
+
 WITH seller_delivered AS (
   SELECT
     s.seller_id,
     s.seller_state,
-    COUNT(DISTINCT CASE WHEN o.order_status = 'delivered' THEN o.order_id END) AS delivered_orders
+    COUNT(DISTINCT o.order_id) AS delivered_orders
   FROM sellers s
-  LEFT JOIN order_items oi
-    ON oi.seller_id = s.seller_id
-  LEFT JOIN orders o
-    ON o.order_id = oi.order_id
+  LEFT JOIN order_items oi ON oi.seller_id = s.seller_id
+  LEFT JOIN orders o ON o.order_id = oi.order_id AND o.order_status = 'delivered'
   GROUP BY s.seller_id, s.seller_state
 )
+
+-- output states with 0 deliveries and count how many
 SELECT
   seller_state,
   COUNT(seller_id) AS sellers_never_delivered
